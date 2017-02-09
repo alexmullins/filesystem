eb9b337f6a53cfa7f0837a828de8133995aab74d
alexmullins <jalexmullins91@gmail.com>
2017-02-08 22:47:53 -0600

    Added logging

--- filesystem/main.c
+++ filesystem/main.c
@@ -5,17 +5,22 @@ int main(int argc, char** argv) {
 	printf("Hello world\n");
 
 	// Test Read and Write
+	printf("Reading word\n");
 	int32_t ret = ReadWord(0x00000000);
 	if (ret < 0) {
 		printf("Couldn't ReadWord, %d\n", ret);
 		return 1;
 	}
 	printf("Val: %d\n", (uint16_t)ret);
+
+	printf("Writing a word\n");
 	ret = WriteWord(0, 30);
 	if (ret < 0) {
 		printf("Couldn't WriteWord: %d\n", ret);
 		return 1;
 	}
+
+	printf("Reading word\n");
 	ret = ReadWord(0);
 	if (ret < 0) {
 		printf("Couldn't ReadWord back out, %d\n", ret);
@@ -24,11 +29,14 @@ int main(int argc, char** argv) {
 	printf("ReadWord: %d\n", ret);
 
 	// Test EraseAll
+	printf("Erasing all sectors\n");
 	ret = EraseAllSectors();
 	if (ret < 0) {
 		printf("Couldn't EraseAllSectors, %d\n", ret);
 		return 1;
 	}
+
+	printf("Reading word\n");
 	ret = ReadWord(0);
 	if (ret < 0) {
 		printf("Couldn't ReadWord, %d\n", ret);
